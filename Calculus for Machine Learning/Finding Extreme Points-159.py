101750cea28d8f8415d03912b7cedcb439512f0e
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-27 23:57:46 +0530

    Add files via upload

---
+++ Calculus for Machine Learning/Finding Extreme Points-159.py
@@ -0,0 +1,28 @@
+## 3. Differentiation ##
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.linspace(0, 3, 100)
+y = -1*(x**2) + x*3 - 1
+plt.plot(x,y)
+
+## 6. Power Rule ##
+
+derivative_one = "5x^4"
+derivative_two = "9x^8"
+slope_one = 80
+slope_two = 0
+
+## 7. Linearity Of Differentiation ##
+
+derivative_three = "5x^4 - 1"
+derivative_four = "3x^2 - 2x"
+slope_three = 4
+slope_four = 8
+
+## 8. Practicing Finding Extreme Values ##
+
+derivative = "3x^2 - 2x"
+critical_points = [0, 2/3]
+rel_min = [2/3]
+rel_max = [0]
